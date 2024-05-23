2ee15a29c353553e4a71083934c0667021f9ed84
Bob Caslin <bob.caslin@gmail.com>
2024-05-23 19:09:36 -0400

    Initial Commit

---
+++ MyPortfolioApp/MyPortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortfolioAppApp.swift
+//  MyPortfolioApp
+//
+//  Created by Robert Caslin on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
